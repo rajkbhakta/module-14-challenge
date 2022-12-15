08682cb08fa201bf5a5d8834cd211cb03aa22b7b
thaiscmky <thaiscmky@github.com>
2022-12-15 03:21:21 -0500

    Adds solutions for W10D3

--- 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Letter.js
+++ 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Letter.js
@@ -1,5 +1,26 @@
 class Letter {
-  
+   constructor(letter) {
+        this.visible = false;
+        this.letter = letter;
+   }
+
+   guess(letter) {
+    //if the letter is in the word
+    //then, set the visibility of this letter to true
+    //otherwise, it remains false
+   }
+
+   toString() {
+    //check if letter was guessed, and return letter
+     return 'guessed-letter';
+   }
+
+   getSolution() {
+    //logic letter
+    return 'some-character'
+   }
+
+
 }
 
 module.exports = Letter;
