09fb702a4ad2490aa2687e9a30100ce2f3964ce4
thaiscmky <thaiscmky@github.com>
2023-01-10 18:46:09 -0500

    Adds activities for W13D2

--- 13-ORM/01-Activities/13-Ins_Validation/server.js
+++ 13-ORM/01-Activities/13-Ins_Validation/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-
-// Import the connection object
+const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -9,7 +8,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connect to the database before starting the Express.js server
-sequelize.sync({force: false}).then(() => {
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
