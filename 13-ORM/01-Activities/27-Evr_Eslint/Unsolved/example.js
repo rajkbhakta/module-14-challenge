a8dcae49078a13447daa0b5451ffa409bae47b61
thaiscmky <thaiscmky@github.com>
2023-01-13 08:50:23 -0500

    Adds solutions for W13D3

--- 13-ORM/01-Activities/27-Evr_Eslint/Unsolved/example.js
+++ 13-ORM/01-Activities/27-Evr_Eslint/Unsolved/example.js
@@ -1,13 +1,13 @@
-var num_1 =  parseInt(prompt('Give me a number!'))
-var num_2 = parseInt(prompt("Give me another number!"))
-var result;
+let num_1 =  parseInt(prompt("Give me a number!"));
+let num_2 = parseInt(prompt("Give me another number!"));
+let result;
 
-var operation = prompt('What would you like to do? (add, subtract, multiply, divide)').toUpperCase();
+const operation = prompt("What would you like to do? (add, subtract, multiply, divide)").toUpperCase();
 
-if (operation == "ADD") {
-     var result = num_1 + num_2;
-  alert('The sum of ' + num_1 + " and " + num_2 + " is " + result);
-} else if (   operation === 'SUBTRACT') 
+if (operation === "ADD") {
+     let result = num_1 + num_2;
+  alert("The sum of " + num_1 + " and " + num_2 + " is " + result);
+} else if (   operation === 'SUBTRACT')
 {
 result = num_1 - num_2;
 alert('The difference between ' + num_1 + " and " + num_2 + " is " + result);
