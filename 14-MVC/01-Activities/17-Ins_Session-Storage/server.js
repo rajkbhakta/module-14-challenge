9cc3a1ae11dd61fa0b9f14cc892bd8d1c292621e
thaiscmky <thaiscmky@github.com>
2023-01-20 16:02:31 -0500

    Adds activities solutions for W14D2

--- 14-MVC/01-Activities/17-Ins_Session-Storage/server.js
+++ 14-MVC/01-Activities/17-Ins_Session-Storage/server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const connect_session_seq = require('connect-session-sequelize');
+const SequelizeStore = connect_session_seq(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
